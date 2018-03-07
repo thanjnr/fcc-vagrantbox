969a3f524e9100fdade2a01f935d2d32b74b2c38
Thomas Han <ethomas.han@gmail.com>
2018-03-08 11:45:07 +1300

    Install mongodb and setup ports

--- Vagrantfile
+++ Vagrantfile
@@ -1,15 +1,12 @@
 Vagrant.configure("2") do |config|
   config.vm.provision "shell", path: "provision.sh"
   config.vm.provider :virtualbox do |vb|
-    vb.customize [
-      "setextradata",
-      :id,
-      "VBoxInternal2/SharedFoldersEnableSymlinksCreate/v-root",
-      "1"
-    ]
+    vb.customize ["setextradata", :id, "VBoxInternal2/SharedFoldersEnableSymlinksCreate//vagrant","1"]
   end
   config.vm.define "dev" do |dev|
       dev.vm.box = "ubuntu/xenial64"
       dev.vm.hostname = "fcc-dev"
+      dev.vm.network "forwarded_port", guest: 3000, host: 3000, id: "fcc"
   end
+
 end
--- provision.sh
+++ provision.sh
@@ -8,20 +8,23 @@ dpkg -s npm &>/dev/null || {
     apt-get -y install nodejs build-essential
 }
 
+#install mongodb
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
+apt-get -y update
+sudo apt-get install -y mongodb-org
+systemctl enable mongod.service
+
 #install mailhog
 echo ">>> Installing Mailhog"
 wget --quiet -O ~/mailhog https://github.com/mailhog/MailHog/releases/download/v1.0.0/MailHog_linux_amd64
 # Make it executable
 chmod +x ~/mailhog
 
-cp /vagrant/upstart/mailhog.conf /etc/init/mailhog.conf
-
 #clone freeCodeCamp repository
-sudo -u vagrant -i sh -c `    
-    cd /vagrant;
-    git clone https://github.com/thanjnr/freeCodeCamp.git;
-    cd /vagrant/freeCodeCamp;
-    git remote add upstream https://github.com/freeCodeCamp/freeCodeCamp.git;
-    git checkout staging;
-    npm install --no-bin-links;
-`
+sudo -u vagrant -i sh -c 'cd /vagrant;git clone https://github.com/thanjnr/freeCodeCamp.git'   
+sudo -u vagrant -i sh -c 'cd /vagrant/freeCodeCamp;git remote add upstream https://github.com/freeCodeCamp/freeCodeCamp.git;git checkout staging'   
+sudo -u vagrant -i sh -c 'cd /vagrant/freeCodeCamp; npm install'
+sudo -u vagrant -i sh -c 'cp /vagrant/freeCodeCamp/sample.env /vagrant/freeCodeCamp/.env'
+cp /vagrant/upstart/mailhog.conf /etc/init/mailhog.conf
+sudo -u vagrant -i sh -c 'cd /vagrant/freeCodeCamp; npm run only-once'
